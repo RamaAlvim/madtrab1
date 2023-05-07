9625c4fa4f8fe4e2444ad214b93edd249acc15dd
Rama Alvim Sales Schiavo <alvimrama@gmail.com>
2023-05-07 17:46:11 -0300

    manual kmeans feito. classificaçao por rmse pendente automatizar kmeans ok artigo Eduar adiantou plotar adequadamente todos as figuras, destacando centroide dos municipios por cor e tamanho, mas nao separando destintamente cada uma delas.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import pdb
 import sys
+
+import matplotlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +10,10 @@ import matplotlib.axes as ax
 import sklearn.cluster
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-from matplotlib import pyplot as plt #para plotar os gráficos
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
 import csv
 
 # open csv
@@ -25,22 +30,6 @@ with open('./municipios_mg.csv','r') as csvfile:
         lat.append(float(row[4]))
         lon.append(float(row[3]))
 
-# #get min/max values to plot
-# minlon= float(min(lon))
-# maxlon= float(max(lon))
-#
-# minlat= float(min(lat))
-# maxlat= float(max(lat))
-
-# plt.scatter(lon, lat)
-# plt.scatter(lon,lat, s=5) # esse!!
-
-# get columns with, long and lat
-# datanew = data.iloc[:, [ 3, 4]]
-# sys.exit(1)
-# import pdb; pdb.set_trace();
-
-#concatenate arrays
 lonnp = np.array(lon)
 
 latnp = np.array(lat)
@@ -49,16 +38,16 @@ combined = np.transpose((lonnp, latnp))
 print(combined)
 # sys.exit(1)
 #kmeans
-
-kmeans = KMeans(n_clusters=4, random_state=0, n_init="auto").fit(combined)
-
-centroidk2 = kmeans.cluster_centers_
-
-
-plt.scatter(centroidk2[0][0],centroidk2[0][1], color='red')
-plt.scatter(centroidk2[1][0],centroidk2[1][1], color='red')
-plt.scatter(centroidk2[2][0],centroidk2[2][1], color='red')
-plt.scatter(centroidk2[3][0],centroidk2[3][1], color='red')
-
-plt.show()
+results = []
+# list with clusters qtd
+clustersQtd = list(range(2, 11))
+for clusterqt in clustersQtd:
+    print(clusterqt)
+    cluster = KMeans(n_clusters=clusterqt, random_state=0, n_init="auto")
+    cluster.fit(combined)
+    #
+    plt.scatter(lon, lat, s=5)  # esse!!
+    plt.scatter(cluster.cluster_centers_[:, 0], cluster.cluster_centers_[:, 1], c='red')
+    plt.show()
+    #inserir o valor de métrica do resultado no results junto com a quantidade de centroides clusterqt
 # import pdb; pdb.set_trace();
